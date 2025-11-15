4ecbb19904c9c6a0aec417ef970a573527ad2610
KokoroHoshi <kokorohoshi2@gmail.com>
2025-11-15 00:16:13 +0000

    Refactor: move Backtester into core.

--- src/core/Trade.h
+++ src/core/Trade.h
@@ -4,18 +4,18 @@
 #include <string>
 
 enum class TradeType {
-BUY,
-SELL
+    BUY,
+    SELL
 };
 
 struct Trade {
-std::string date;
-TradeType type;
-double price;
-double quantity;
+    std::string date;
+    TradeType type;
+    double price;
+    double quantity;
 
-Trade(const std::string& date, TradeType type, double price, double quantity)
-: date(date), type(type), price(price), quantity(quantity) {}
+    Trade(const std::string& date, TradeType type, double price, double quantity)
+        : date(date), type(type), price(price), quantity(quantity) {}
 };
 
 #endif
